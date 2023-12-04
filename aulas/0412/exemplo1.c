ea87d18793287c11d7a4d0ee6ef149830aa8df7b
hillary <hillary.saldanha199@gmail.com>
2023-12-04 15:35:13 -0300

    Exemplo 1 dos slides.

---
+++ aulas/0412/exemplo1.c
@@ -0,0 +1,12 @@
+void incrementa(int *ponteiro){
+
+    (*ponteiro)++;
+}
+
+int main() {
+
+    int variavel = 5;
+    incrementa(&variavel);
+    printf("Valor da variavel: %d", variavel);
+    return 0;
+}
